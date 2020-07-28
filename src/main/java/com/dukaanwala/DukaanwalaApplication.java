9cd06bdbb26d7108c8e896615fce09f0a1408d37
1sujeet1 <software.er.sujeet@gmail.com>
2020-07-28 18:22:13 +0530

    first-initial commit

---
+++ src/main/java/com/dukaanwala/DukaanwalaApplication.java
@@ -0,0 +1,13 @@
+package com.dukaanwala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DukaanwalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DukaanwalaApplication.class, args);
+	}
+
+}
